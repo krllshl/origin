22c06c8592e38de5bb4be4b58197f06177d60157
Kirill Shelkovich <mjjjaga@gmail.com>
2018-10-21 20:14:21 +0200

    first commit branch

--- main.c
+++ main.c
@@ -4,5 +4,7 @@ int main(void)
 {
     printf("Hello. I'm your first prj!");
     printf("Hello, I don't think that it is a good idea.");
+    for (int i=0;i<10;i++)
+	printf("BYE!");
     return 0;
 }
